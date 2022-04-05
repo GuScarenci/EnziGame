920cee5fa2692f1482f756f95d145cd3abd711f8
Gustavo Moura Scarenci <gustavomourascarenci@usp.br>
2022-04-04 23:11:12 -0300

    Background Updates

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,23 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
     int level;
-
-    public GameObject um;
-    public GameObject dois;
     TeacherSpeakManager teacherScript;
     public SideSliderController sliderScript;
+    public Sprite[] backGroundSprite;
+
 
     public PlayerController playerScript;
 
     public TMP_Text levelText; 
 
+    GameObject[,] background = new GameObject[40,40];
+    int backgroundCount = 40;
+    public GameObject backgroundPrefab;
+    SpriteRenderer spriteRenderer;
+
     void Start()
     {
         teacherScript = this.gameObject.GetComponent<TeacherSpeakManager>();
+        for(int i = 0;i<backgroundCount;i++){
+            for(int j = 0;j<backgroundCount;j++){
+                background[i,j] = Instantiate(backgroundPrefab, new Vector3(i*20 -100, j*20 - 100, 0), Quaternion.Euler(new Vector3(0, 0, Random.Range(1,5)*90)));
+                spriteRenderer = background[i,j].GetComponent<SpriteRenderer>();
+                spriteRenderer.sprite = backGroundSprite[0];
+            }
+        }
     }
 
     void Update()
@@ -39,7 +51,12 @@ public class LevelManager : MonoBehaviour
 
             teacherScript.ChangeLine();
 
-            um.SetActive(false);
+            for(int i = 0;i<backgroundCount;i++){
+                for(int j = 0;j<backgroundCount;j++){
+                    spriteRenderer = background[i,j].GetComponent<SpriteRenderer>();
+                    spriteRenderer.sprite = backGroundSprite[1];
+                }
+            }
             
         }else if(level == 1){
             sliderScript.ShowSlider(0);
@@ -50,12 +67,21 @@ public class LevelManager : MonoBehaviour
 
             teacherScript.ChangeLine();
 
-            dois.SetActive(false);
+            for(int i = 0;i<backgroundCount;i++){
+                for(int j = 0;j<backgroundCount;j++){
+                    spriteRenderer = background[i,j].GetComponent<SpriteRenderer>();
+                    spriteRenderer.sprite = backGroundSprite[2];
+                }
+            }
         }
         
         level++;
         teacherScript.level = level;
         playerScript.level = level;
-        levelText.text = "Fase " + (level + 1);
+        if(level == 1){
+            levelText.text = "Estômago";
+        }else if(level == 2){
+            levelText.text = "Intestino";
+        }
     }
 }
